e0132c9b57582a2259da1ea95739c9ffe731cbf6
ankeett <94247776+ankeett@users.noreply.github.com>
2022-10-01 10:33:17 -0400

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,13 +4,33 @@ import {RiCloseLine} from 'react-icons/ri'
 import { links } from '../assets/constants';
 import {logo} from '../assets';
 
+
+  const NavLinks = ({handleClick}) => (
+  <div className='mt-10'>
+    {links.map((item)=>(
+      <NavLink 
+      key={item.name} 
+      to={item.to}
+      className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
+      onClick={()=>handleClick && handleClick()}
+      >
+        {item.name}
+      </NavLink>
+    ))}
+
+  </div>
+  )
+
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
    return(
     <>
       <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
         <img src={logo} alt="logo" className='w-full h-25' />
+        <NavLinks/>
+        
       </div>
+      
     </>
    )
    };
