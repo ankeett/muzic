b459b2a077875d821dc01fffafe456e79dd542c4
ankeett <94247776+ankeett@users.noreply.github.com>
2022-10-01 09:45:18 -0400

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,18 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import {useState} from 'react'
+import { NavLink} from 'react-router-dom';
+import {RiCloseLine} from 'react-icons/ri'
+import { links } from '../assets/constants';
+import {logo} from '../assets';
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+   return(
+    <>
+      <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]'>
+        <img src={logo} alt="logo" className='w-full h-25' />
+      </div>
+    </>
+   )
+   };
 
 export default Sidebar;
